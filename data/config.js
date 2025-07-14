8f8e7e9ab3b5403d90dd2aa0777734c63ee1a689
kimjunodes <rla04790@gmail.com>
2025-07-14 22:31:18 +0900

    [Modify] draft_html#1_20250714

---
+++ data/config.js
@@ -0,0 +1,47 @@
+var config = {
+    title: "[소통과 공감으로 조직을 활성화하라]",
+    // menu_study_sub_index: false, //menu study sub-index
+    // content_path: "../content/",
+    // competency_test: true,
+    // competency_test_path: "../CompetencyTest/index.html",
+    // cookie_expire: 1, //cookie expire day
+    // summary_path: "../document/summary/",
+    // learning_resources: true, // 학습자료 사용여부
+    // learning_resources_path: "../document/learning_resources/",
+    // learning_resources_list: [{
+    //     title: "보충심화학습자료",
+    //     file_name: "supplementary_learning.pdf"
+    // }], // 학습자료 리스트
+    page_type: {
+        "1": {
+            title: "인트로",
+            type: "",
+            content_extension: "mp4"
+        },
+        "2": {
+            title: "학습내용+학습목표",
+            type: "",
+            content_extension: "mp4"
+        },
+        "3": {
+            title: "강의 본문",
+            type: "study",
+            content_extension: "mp4"
+        },
+        "4": {
+            title: "평가하기",
+            type: "quiz",
+            content_extension: "mp3"
+        },
+        "5": {
+            title: "정리하기",
+            type: "summary",
+            content_extension: "mp3"
+        },
+        "6": {
+            title: "Outro",
+            type: "",
+            content_extension: "mp4"
+        }
+    }
+};
